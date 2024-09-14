ecb9b77a8fd5c435b073c912e1a4759e1d6ae8f8
tejasgsdekar-9 <tejasgadekar108@gmail.com>
2024-09-14 10:25:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,9 +69,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("hi, i am root");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 
 app.use(session(sessionOptions));
